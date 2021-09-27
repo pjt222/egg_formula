486ec0df74065eb84dfa75a6e66f4d40593fcf91
Philipp Thoss <ph.thoss@gmx.de>
2021-09-27 14:35:34 +0200

    found well working parameters and refined plot

--- egg_coords.R
+++ egg_coords.R
@@ -1,7 +1,7 @@
-L <- 6 # Eilänge
-w <- 4 # maximalen Breite
-B <- .5 # Abstand zwischen dem Bereich der maximalen Breite und der halben Länge des Eies
-DL4 <- 3 # Eidurchmesser (ein Viertel der Eilänge vom spitzen Ende entfernt)
+L <- 8 # Eilänge
+w <- 7 # maximalen Breite
+B <- 3.5 # Abstand zwischen dem Bereich der maximalen Breite und der halben Länge des Eies
+DL4 <- 7 # Eidurchmesser (ein Viertel der Eilänge vom spitzen Ende entfernt)
 
 Term1 <- function(x, ...) {
   # TODO mind + -
@@ -35,31 +35,48 @@ Term2 <- 1 - Term21(x) * Term22(x)
 res <- Term1(x) * Term2
 z <- res
 
-plot(x = x, y = res)
+# plot(x = x, y = res)
+
+egg_sclice <- data.frame(
+  x = x, 
+  y_p = res,
+  y_n = -res
+  ) %>% 
+  pivot_longer(
+    cols = starts_with("y_"),
+    names_to = "y_direction",
+    values_to = "y"
+  )
 
 ggplot(
-    data = data.frame(x = x, y = res),
-    aes(x = x, y = y)
+    data = egg_sclice,
+    aes(
+      x = x, 
+      y = y,
+      color = y_direction
+      )
   )+
   geom_point() +
   scale_x_continuous(limits = c(-L/2-1, L/2+1)) +
-  scale_y_continuous(limits = c(-L/2-1, L/2+1))
+  scale_y_continuous(limits = c(-L/2-1, L/2+1)) +
+  scale_colour_viridis_d(guide = NULL) +
+  theme_dark()
 
-rgl::plot3d(
-  x = x,
-  y = res,
-  z = -1:1,
-  col = "#696969" #rainbow(1000)
-)
-
-y2 <- function(x,...) {
-  B/2*((L^2-4*x^2)/L)^.5*(
-    (65.22*(w/L)^2-4.57*w/L+0.4+(102.61*(w/L)^2-13.67*w/L-0.39)*x/L+(x/L)^2) / 
-      (107.91*(w/L)^2 - 9.16*w/L + 0.49+(375.88*(w/L)^2-39.6*w/L+0.31)*x/L+(302.37*(w/L)^2-41.83*w/L+2.07)*(w/L)^2)
-  )^.5
-  
-}
-
-y2s <- y2(seq(-L / 2, L / 2, by = .001))
-
-plot(x, y2s)
+# rgl::plot3d(
+#   x = x,
+#   y = res,
+#   z = -1:1,
+#   col = "#696969" #rainbow(1000)
+# )
+# 
+# y2 <- function(x,...) {
+#   B/2*((L^2-4*x^2)/L)^.5*(
+#     (65.22*(w/L)^2-4.57*w/L+0.4+(102.61*(w/L)^2-13.67*w/L-0.39)*x/L+(x/L)^2) / 
+#       (107.91*(w/L)^2 - 9.16*w/L + 0.49+(375.88*(w/L)^2-39.6*w/L+0.31)*x/L+(302.37*(w/L)^2-41.83*w/L+2.07)*(w/L)^2)
+#   )^.5
+#   
+# }
+# 
+# y2s <- y2(seq(-L / 2, L / 2, by = .001))
+# 
+# plot(x, y2s)
