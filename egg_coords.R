427a71478df9b19c53a09d44b6e325243027452f
Philipp Thoss <ph.thoss@gmx.de>
2021-10-14 12:45:48 +0200

    great cleanup

--- egg_coords.R
+++ egg_coords.R
@@ -1,6 +1,8 @@
+# libraries ----
 library(tidyverse)
 library(rgl)
 
+# constants ----
 L <- 8 # Eilänge
 w <- 3.5 * 2 # maximalen Breite
 B <- 1.7 * 2 # Abstand zwischen dem Bereich der maximalen Breite und der halben Länge des Eies
@@ -31,6 +33,7 @@ Term22 <- function(x, ...) {
   )^.5
 }
 
+# create data ----
 Term2 <- 1 - Term21(x) * Term22(x)
 
 res <- Term1(x) * Term2
@@ -69,97 +72,7 @@ all_egg_slices <- full_join(
   select(x, y, z) %>% 
   distinct()
 
-
-# TODO https://stackoverflow.com/questions/67236291/how-to-rotate-vector-time-series
-
-rotx <- function(a # in radians, e.g. a = pi/4
-) {
-  # https://www.mathworks.com/help/phased/ref/rotx.html
-  # Counterclockwise rotation around x-axis
-  matrix(
-    c(
-      1, 0, 0,
-      0, cos(a), -sin(a),
-      0, sin(a), cos(a)
-    ),
-    nrow = 3,
-    ncol = 3,
-    byrow = TRUE
-  )
-}
-
-rotz <- function(a # in radians, e.g. a = pi/4
-) {
-  # https://www.mathworks.com/help/phased/ref/rotx.html
-  # Counterclockwise rotation around x-axis
-  matrix(
-    c(
-      cos(a), -sin(a), 0,
-      sin(a), cos(a), 0,
-      0, 0, 1
-    ),
-    nrow = 3,
-    ncol = 3,
-    byrow = TRUE
-  )
-}
-
-rots <- function(a # in radians, e.g. a = pi/4
-) {
-  matrix(
-    c(
-      cos(a), -sin(a),
-      sin(a), cos(a)
-    ),
-    nrow = 2,
-    ncol = 2,
-    byrow = F
-  )
-}
-
-egg_rotated <- lapply(1:NROW(egg_slice), function(es) {
-  new_coords <- lapply(seq.default(0, 2, .25), function(theta) {
-    rotated <- c(unname(unlist(egg_slice[es, c("x", "y")])), 0) * rotx(theta)
-
-    data.frame(
-      x = round(rowSums(rotated), 2)[1],
-      y = round(rowSums(rotated), 2)[2],
-      z = round(rowSums(rotated), 2)[3],
-      theta = theta
-    )
-  })
-  new_coords <- bind_rows(new_coords)
-})
-
-egg_rotated <- bind_rows(egg_rotated) %>%
-  select(-theta) %>%
-  distinct()
-
-ggplot(
-  data = egg_slice,
-  aes(
-    x = x,
-    y = y,
-    color = y_direction,
-    fill = y_direction,
-    alpha = .5
-  )
-) +
-  geom_point() +
-  scale_x_continuous(limits = c(-L / 2 - 1, L / 2 + 1)) +
-  scale_y_continuous(limits = c(-L / 2 - 1, L / 2 + 1)) +
-  scale_colour_viridis_d(guide = NULL) +
-  scale_fill_viridis_d(guide = NULL) +
-  scale_alpha(guide = NULL) +
-  theme_dark()
-
-plot3d(
-  x = egg_rotated$x, # egg_slice$x,
-  y = egg_rotated$y, # egg_slice$y,
-  z = egg_rotated$z, # 0,#sin(egg_slice$y*2*pi),
-  col = "#696969" # rainbow(1000)
-)
-
+# plot ----
 plot3d(
   x = slice_to_circle$x,
   y = slice_to_circle$y_slice,
@@ -181,16 +94,3 @@ plot3d(
   box = FALSE,
   add = FALSE
 )
-
-#
-# y2 <- function(x,...) {
-#   B/2*((L^2-4*x^2)/L)^.5*(
-#     (65.22*(w/L)^2-4.57*w/L+0.4+(102.61*(w/L)^2-13.67*w/L-0.39)*x/L+(x/L)^2) /
-#       (107.91*(w/L)^2 - 9.16*w/L + 0.49+(375.88*(w/L)^2-39.6*w/L+0.31)*x/L+(302.37*(w/L)^2-41.83*w/L+2.07)*(w/L)^2)
-#   )^.5
-#
-# }
-#
-# y2s <- y2(seq(-L / 2, L / 2, by = .001))
-#
-# plot(x, y2s)
