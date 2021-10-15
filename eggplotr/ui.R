0d88cbcc33ae7672826adb93cc83ba15c82c58ab
Philipp Thoss <ph.thoss@gmx.de>
2021-10-15 16:43:07 +0200

    more widgets for more color

--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -3,10 +3,13 @@ library(rgl)
 library(shinyWidgets)
 
 fluidPage(
+  titlePanel("eggplotr"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("L", "Length", 1, 10, 8, .5),
-      sliderInput("w", "w", 1, 10, 7, .5),
+      chooseSliderSkin(skin = "Shiny", color = "#696969"),
+      # setSliderColor(rep("#000000",6), c(1:6)),
+      sliderInput("L", "Length", 1, 20, 8, .5),
+      sliderInput("w", "w", 1, 20, 7, .5),
       sliderInput("B", "B", 1, 10, 3.5, .5),
       sliderInput("DL4", "DL4", 1, 10, 5, .5),
       sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01),
