0ef89f05c292143c027e6a166d54cbc075b42d39
Philipp Thoss <ph.thoss@gmx.de>
2021-10-15 14:30:58 +0200

    cleanup

--- eggplotr/server.R
+++ eggplotr/server.R
@@ -1,26 +1,20 @@
-# Must be executed BEFORE rgl is loaded on headless devices.
-# options(rgl.useNULL = TRUE)
-
 library(shiny)
 library(rgl)
 library(tidyverse)
 
 source("../egg_coords.R")
 
-shinyServer(
-  function(input, output) {
-    output$egg <- renderRglwidget({
-      plot_egg(
-        L = input$L,
-        w = input$w,
-        B = input$B,
-        DL4 = input$DL4,
-        seq01 = input$seq01,
-        seq02 = input$seq02,
-        color = input$color
-      )
-      rglwidget()
-      # rgl.close()
-    })
-  }
-)
+function(input, output) {
+  output$egg <- renderRglwidget({
+    plot_egg(
+      L = input$L,
+      w = input$w,
+      B = input$B,
+      DL4 = input$DL4,
+      seq01 = input$seq01,
+      seq02 = input$seq02,
+      color = input$color
+    )
+    rglwidget()
+  })
+}
--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -1,22 +1,19 @@
 library(shiny)
 library(rgl)
 
-shinyUI(
-  fluidPage(
+fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("L","Length", 1, 10, 8, .5),
+      sliderInput("L", "Length", 1, 10, 8, .5),
       sliderInput("w", "w", 1, 10, 7, .5),
-      sliderInput("B", "B" , 1, 10, 3.5, .5),
-      sliderInput("DL4", "DL4",1, 10, 5, .5),
-      sliderInput("seq01","seq01",0.01, 0.2, 0.05, .01),
-      sliderInput("seq02","seq02",1, 15, 2, 1),
-      textInput("color","color", "black")
+      sliderInput("B", "B", 1, 10, 3.5, .5),
+      sliderInput("DL4", "DL4", 1, 10, 5, .5),
+      sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01),
+      sliderInput("seq02", "seq02", 1, 15, 2, 1),
+      textInput("color", "color", "orange")
     ),
     mainPanel(
       rglwidgetOutput("egg", width = "900px", height = "900px")
     )
-  )  
-    
   )
 )
