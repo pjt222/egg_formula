be3fed0ea0805b24f842cb4e5677ab4d97519832
Philipp Thoss <ph.thoss@gmx.de>
2021-10-15 14:16:23 +0200

    wip

--- eggplotr/server.R
+++ eggplotr/server.R
@@ -11,15 +11,16 @@ shinyServer(
   function(input, output) {
     output$egg <- renderRglwidget({
       plot_egg(
-        L = input$L,
-        w = input$w,
-        B = input$B,
-        DL4 = input$DL4,
-        seq01 = input$seq01,
-        seq02 = input$seq02,
-        color = input$color
-      )
+          L = input$L,
+          w = input$w,
+          B = input$B,
+          DL4 = input$DL4,
+          seq01 = input$seq01,
+          seq02 = input$seq02,
+          color = input$color
+          )
       rglwidget()
+      # rgl.close()
     })
   }
 )
--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -9,7 +9,7 @@ shinyUI(
       sliderInput("w", "w", 1, 10, 7, .5),
       sliderInput("B", "B" , 1, 10, 3.5, .5),
       sliderInput("DL4", "DL4",1, 10, 5, .5),
-      sliderInput("seq01","seq01",0.01, 0.2, 0.05, .05),
+      sliderInput("seq01","seq01",0.01, 0.2, 0.05, .01),
       sliderInput("seq02","seq02",1, 15, 2, 1),
       textInput("color","color", "black")
     ),
