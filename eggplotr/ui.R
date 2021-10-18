edbcf7691b616e27b01937a9ef0df3ecf5709598
Philipp Thoss <ph.thoss@gmx.de>
2021-10-18 09:31:32 +0200

    adding css with tag function

--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -6,7 +6,7 @@ fluidPage(
   titlePanel("eggplotr"),
   sidebarLayout(
     sidebarPanel(
-      chooseSliderSkin(skin = "Shiny", color = "#696969"),
+      chooseSliderSkin(skin = "Flat", color = "orange"),
       # setSliderColor(rep("#000000",6), c(1:6)),
       sliderInput("L", "Length", 1, 20, 8, .5),
       sliderInput("w", "w", 1, 20, 7, .5),
@@ -14,7 +14,12 @@ fluidPage(
       sliderInput("DL4", "DL4", 1, 10, 5, .5),
       sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01),
       sliderInput("seq02", "seq02", 1, 15, 2, 1),
-      textInput("color", "color", "orange")
+      textInput("color", "color", "orange"),
+      tags$style(".well {
+                 background-color:#696969;
+                 border-color:#696969;
+                 border-width:0px;
+                 }")
     ),
     mainPanel(
       rglwidgetOutput("egg", width = "900px", height = "900px")
