ae4de4a4d8135a06f967a372a1efc2c11e559ed6
Philipp Thoss <ph.thoss@gmx.de>
2021-10-18 16:26:13 +0200

    Slider animation and no ticks fro zen setup

--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -10,12 +10,12 @@ fluidPage(
     sidebarPanel(
       chooseSliderSkin(skin = "Shiny", color = "#696969 !important; border-color: #000000"),
       # setSliderColor(rep("#000000",6), c(1:6)),
-      sliderInput("L", "Length", 1, 20, 8, .5),
-      sliderInput("w", "w", 1, 20, 7, .5),
-      sliderInput("B", "B", 1, 10, 3.5, .5),
-      sliderInput("DL4", "DL4", 1, 10, 5, .5),
-      sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01),
-      sliderInput("seq02", "seq02", 1, 15, 2, 1),
+      sliderInput("L", "Length", 1, 20, 8, .5, ticks = FALSE, animate = TRUE),
+      sliderInput("w", "w", 1, 20, 7, .5, ticks = FALSE, animate = TRUE),
+      sliderInput("B", "B", 1, 10, 3.5, .5, ticks = FALSE, animate = TRUE),
+      sliderInput("DL4", "DL4", 1, 10, 5, .5, ticks = FALSE, animate = TRUE),
+      sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01, ticks = FALSE, animate = TRUE),
+      sliderInput("seq02", "seq02", 1, 15, 2, 1, ticks = FALSE, animate = TRUE),
       colourInput("color", "color", "orange"),
       tags$style("
                  .well {
