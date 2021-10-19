7df0fe66243329555b0071ad825eaf9a6c08f302
Philipp Thoss <ph.thoss@gmx.de>
2021-10-19 10:18:15 +0200

    slider styling with less complex structure

--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -8,23 +8,30 @@ fluidPage(
   titlePanel("eggplotr"),
   sidebarLayout(
     sidebarPanel(
-      chooseSliderSkin(skin = "Shiny", color = "#696969 !important; border-color: #000000"),
-      # setSliderColor(rep("#000000",6), c(1:6)),
+      tags$head(
+        tags$style("
+          .irs-bar-edge, .irs-bar, .irs-single, .irs-from, .irs-to
+            {
+              background: #696969 !important;
+              border-color: #000000 !important;
+              border-width: 2px !important;
+            }
+          .well
+            {
+              background-color:#696969;
+              border-style:none;
+              border-color:#696969;
+              border-width:0px;
+            }
+          ")
+      ),
       sliderInput("L", "Length", 1, 20, 8, .5, ticks = FALSE, animate = TRUE),
       sliderInput("w", "w", 1, 20, 7, .5, ticks = FALSE, animate = TRUE),
       sliderInput("B", "B", 1, 10, 3.5, .5, ticks = FALSE, animate = TRUE),
       sliderInput("DL4", "DL4", 1, 10, 5, .5, ticks = FALSE, animate = TRUE),
       sliderInput("seq01", "seq01", 0.01, 0.2, 0.05, .01, ticks = FALSE, animate = TRUE),
       sliderInput("seq02", "seq02", 1, 15, 2, 1, ticks = FALSE, animate = TRUE),
-      colourInput("color", "color", "orange"),
-      tags$style("
-                 .well {
-                 background-color:#696969;
-                 border-style:none;
-                 border-color:#696969;
-                 border-width:0px;
-                 }
-                 ")
+      colourInput("color", "color", "orange")
     ),
     mainPanel(
       rglwidgetOutput("egg", width = "900px", height = "900px")
