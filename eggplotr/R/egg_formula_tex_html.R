2e7ee5e8536dde22d75373357ca4c286ce1ae239
Philipp Thoss <ph.thoss@gmx.de>
2022-03-08 13:12:30 +0100

    formula completion

--- eggplotr/R/egg_formula_tex_html.R
+++ eggplotr/R/egg_formula_tex_html.R
@@ -1,6 +1,6 @@
-katex::katex_html(
-"
-  y = \\pm \\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}*
-  (1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}*(\\sqrt{3}BL-2D_{\\frac{L}{4}}}{}*
-  (1-\\sqrt{}))
+katex::katex_html("
+  y = \\pm \\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times
+\\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}
+            {\\sqrt{3}BL\\left(\\sqrt{5.5L^2+11Lw+4w^2}-2\\sqrt{L^2+2wL+4w^2}\\right)}\\times
+\\left(1-\\sqrt{\\frac{L(L^2+8wx+4w^2)}{2(L-2w)x^2+(L^2+8Lw-4w^2)x+2Lw^2+L^2w+L^3}}\\right)\\right)
 ")
