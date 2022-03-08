e394511cd175f89a2e6f939ad8e285939f297672
Philipp Thoss <ph.thoss@gmx.de>
2022-03-08 13:14:40 +0100

    assign to global env

--- eggplotr/R/egg_formula_tex_html.R
+++ eggplotr/R/egg_formula_tex_html.R
@@ -1,4 +1,4 @@
-egg_formula_tex_html <- katex::katex_html(
+egg_formula_tex_html <<- katex::katex_html(
   "
     y = \\pm \\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times
     \\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}
