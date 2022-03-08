a4b48129ef0162cf6c4295057da6e011657dcaa1
Philipp Thoss <ph.thoss@gmx.de>
2022-03-08 13:56:20 +0100

    clean up draft_writing for formula

--- eggplotr/R/egg_formula_tex_html.R
+++ eggplotr/R/egg_formula_tex_html.R
@@ -1,4 +1,4 @@
-egg_formula_tex_html <<- katex::katex_html(
+egg_formula_tex <- c(
   "
     y = \\pm \\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times
     \\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}
@@ -6,3 +6,5 @@ egg_formula_tex_html <<- katex::katex_html(
     \\left(1-\\sqrt{\\frac{L(L^2+8wx+4w^2)}{2(L-2w)x^2+(L^2+8Lw-4w^2)x+2Lw^2+L^2w+L^3}}\\right)\\right)
   "
 )
+
+egg_formula_tex_html <- katex::katex_html(egg_formula_tex)
