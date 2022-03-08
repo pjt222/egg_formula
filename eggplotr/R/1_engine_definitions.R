cb7611d9022cd7894b19408ca8443963c4b23582
Philipp Thoss <ph.thoss@gmx.de>
2022-03-08 13:56:39 +0100

    insert final formula without spaces

--- eggplotr/R/1_engine_definitions.R
+++ eggplotr/R/1_engine_definitions.R
@@ -6,8 +6,9 @@ engine_definition_brain <<- paste0(
     <h2 style='color: black;'>egg-formula</h2>
     ",
     # formula
-    paste0("$$",katex::example_math(),"$$"),
-    # "$$\\alpha^2-\\beta$$",
+    paste0("$$",
+           "y=\\pm\\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times\\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}{\\sqrt{3}BL\\left(\\sqrt{5.5L^2+11Lw+4w^2}-2\\sqrt{L^2+2wL+4w^2}\\right)}\\times\\left(1-\\sqrt{\\frac{L(L^2+8wx+4w^2)}{2(L-2w)x^2+(L^2+8Lw-4w^2)x+2Lw^2+L^2w+L^3}}\\right)\\right)",
+           "$$"),
     "
     <h2 style='color: black;'>engines</h2>
     <p style='color: black;'><strong></strong>There are currently four engines implemented to choose from.</p>
