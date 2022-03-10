592531847978b39f4c016ef0a22fce2dc75d9143
Philipp Thoss <ph.thoss@gmx.de>
2022-03-10 16:30:27 +0100

    wip

--- eggplotr/R/1_engine_definitions.R
+++ eggplotr/R/1_engine_definitions.R
@@ -1,22 +1,31 @@
 engine_definition_brain <<- paste0(
   c(
     "
+    
     <h1 style='color: black;'>Welcome to <em>eggplotr</em></h1>
-    <p style='color: black;'>This app aimes to vizualise the 'egg-formula' presented in <em>Egg and math: introducing a universal formula for egg shape</em> by Valeriy G. Narushin, Michael N. Romanov and Darren K. Griffin (<a href='https://doi.org/10.1111/nyas.14680' style='color: black;' target='_blank' title='Egg and math: introducing a universal formula for egg shape' rel='noopener'>https://doi.org/10.1111/nyas.14680</a>).</p>
+    <p style='color: black;'>This app aimes to introduce and vizualise the 'egg-formula' presented in <em>Egg and math: introducing a universal formula for egg shape</em> by Valeriy G. Narushin, Michael N. Romanov and Darren K. Griffin (<a href='https://doi.org/10.1111/nyas.14680' style='color: black;' target='_blank' title='Egg and math: introducing a universal formula for egg shape' rel='noopener'>https://doi.org/10.1111/nyas.14680</a>).</p>
     <h2 style='color: black;'>egg-formula</h2>
     ",
-    # formula
+    ### formula
     paste0("$$",
-           "y=\\pm\\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times\\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}{\\sqrt{3}BL\\left(\\sqrt{5.5L^2+11Lw+4w^2}-2\\sqrt{L^2+2wL+4w^2}\\right)}\\times\\left(1-\\sqrt{\\frac{L(L^2+8wx+4w^2)}{2(L-2w)x^2+(L^2+8Lw-4w^2)x+2Lw^2+L^2w+L^3}}\\right)\\right)",
+           "y=\\pm\\frac{B}{2}\\sqrt{\\frac{L^2-4x^2}{L^2+8wx+4w^2}}\\times\\\\\\left(1-\\frac{\\sqrt{5.5L^2+11Lw+4w^2}\\times\\left(\\sqrt{3}BL-2D_{\\frac{L}{4}}\\sqrt{L^2+2wL+4w^2}\\right)}{\\sqrt{3}BL\\left(\\sqrt{5.5L^2+11Lw+4w^2}-2\\sqrt{L^2+2wL+4w^2}\\right)}\\times\\\\\\left(1-\\sqrt{\\frac{L(L^2+8wx+4w^2)}{2(L-2w)x^2+(L^2+8Lw-4w^2)x+2Lw^2+L^2w+L^3}}\\right)\\right)",
            "$$"),
+    ###
     "
-    <p style='color: black;'>This formula</p>
+    <h2 style='color: black;'>Parameters</h2>
+    <h3 style='color: black;'>$$L$$</h3>
+    <h3 style='color: black;'>$$w$$</h3>
+    <h3 style='color: black;'>$$B$$</h3>
+    <h3 style='color: black;'>$$D_{\\frac{L}{4}}$$</h3>
+    <h2 style='color: black;'>Parameters: internal</h2>
+    <h3 style='color: black;'>$$seq01$$</h3>
+    <h3 style='color: black;'>$$seq02$$</h3>
     <h2 style='color: black;'>engines</h2>
     <p style='color: black;'><strong></strong>There are currently four engines implemented to choose from.</p>
     <h3 style='color: black;'>b.r.a.i.n.</h3>
     <p style='color: black;'>The first ist the the <em>b.r.a.i.n.</em> engine (<strong>b</strong>iological <strong>r</strong>einforced <strong>a</strong>rtificial <strong>i</strong>ntelligence <strong>n</strong>etwork) wich is owned by every human, but used by less. To use this engine in conjuction with eggplotr just set <em>savant_mode=true&nbsp;</em>and use the preceding formula and explanation to visualize the result with your inner eye.</p>
     <h3 style='color: black;'>rgl and rgl_persp</h3>
-    <p style='color: black;'>The second ('rgl') and third ('rgl_persp') both are based on the R-package <a href='https://dmurdoch.github.io/rgl/' style='color: black;'><em>rgl</em></a> and use the functions <em>plot3d</em> to create a scatterplot or the functions <em>shade3d</em>, <em>turn3d</em> an <em>material3d</em> to create an surface plot.</p>
+    <p style='color: black;'>The second ('rgl') and third ('rgl_persp') both are based on the R-package <a href='https://dmurdoch.github.io/rgl/' style='color: black;'><em>rgl</em></a> and use the function <em>plot3d</em> to create a 3d scatterplot or the functions <em>shade3d</em>, <em>turn3d</em> an <em>material3d</em> to create an 3d surface plot.</p>
     <h3 style='color: black;'>threejs</h3>
     <p style='color: black;'>The fourth is based on the R-package <em><a href='https://cran.r-project.org/web/packages/threejs/threejs.pdf' style='color: black;'>threejs</a>&nbsp;</em>and the function <em>scatterplot3js</em> to create an scatterplot.</p>
     "
--- eggplotr/ui.R
+++ eggplotr/ui.R
@@ -6,7 +6,7 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("e", "engine", c("b.r.a.i.n.","rgl", "rgl_persp", "threejs")),
-      sliderInput("L", "Length", 1, 20, 8, .5, ticks = FALSE), # animate = TRUE),
+      sliderInput("L", "L", 1, 20, 8, .5, ticks = FALSE), # animate = TRUE),
       sliderInput("w", "w", 1, 20, 7, .5, ticks = FALSE), # animate = TRUE),
       sliderInput("B", "B", 1, 10, 3.5, .5, ticks = FALSE), # animate = TRUE),
       sliderInput("DL4", "DL4", 1, 10, 5, .5, ticks = FALSE), # animate = TRUE),
@@ -19,7 +19,7 @@ ui <- fluidPage(
         id = "engine_tabs",
         selected = "b.r.a.i.n.",
         type = "hidden",
-        tabPanel("b.r.a.i.n.", htmlOutput("egg_brain", container = div)),
+        tabPanel("b.r.a.i.n.", htmlOutput("egg_brain", width = "900px", height = "900px")),
         tabPanel("rgl", rglwidgetOutput("egg_rgl", width = "900px", height = "900px")),
         tabPanel("rgl_persp", rglwidgetOutput("egg_rgl_persp", width = "900px", height = "900px")),
         tabPanel("threejs", scatterplotThreeOutput("egg_threejs", width = "900px", height = "900px"))
